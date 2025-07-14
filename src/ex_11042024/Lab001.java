2b6105695eb139ef686ae994e60522d72eeb2487
amolkatke <katkeamol16@gmail.com>
2025-07-14 17:26:13 +0530

    first commit

---
+++ src/ex_11042024/Lab001.java
@@ -0,0 +1,11 @@
+package ex_11042024;
+
+public class Lab001 {
+    public static void main(String[] args) {
+        System.out.println("Amol");
+        System.out.println("katke");  //this are statements
+
+        // Class -> main -> statement
+        //main is method
+    }
+}
